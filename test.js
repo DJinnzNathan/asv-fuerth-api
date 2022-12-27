640e17e9100ded207f786b0f1a804a04cbd6b655
D-Jinnz Nathan <djinnz.nathan@gmail.com>
2022-12-27 15:01:39 +0100

    init express server for api

--- test.js
+++ test.js
@@ -7,36 +7,48 @@ const PORT = process.env.port || 3000;
 
 const website = 'https://www.bfv.de/mannschaften/asv-fuerth/016PJQMDDO000000VV0AG811VUDIC8D7';
 
-try {
-    axios(website).then((res) => {
-        const data = res.data;
-        const $ = cheerio.load(data);
+// try {
+//     axios(website).then((res) => {
+//         const data = res.data;
+//         // console.log(res.data);
+//         const $ = cheerio.load(data);
 
-        let content = [];
+//         let content = [];
 
-        $('.bfv-statistic', data).first(() => {
-            const info = $().find('.bfv-result-tile');
+//         $('.bfv-statistic', data).each(() => {
+//             const lastGame = $().find('.bfv-result-tile');
+//             console.log(lastGame);
+//             content.push({
+//                 lastGame,
+//             });
 
-            content.push({
-                info,
-            });
-
-            app.get('/push', (req, res) => {
-                // res.json(content);
-                res.json(info);
-            });
-        });
-    });
-} catch (error) {
-    console.log(error, error.message);
-}
+//             app.get('/', (req, res) => {
+//                 // res.json(content);
+//                 res.send('Push');
+//                 res.send(info);
+//             });
+//         });
+//     });
+// } catch (error) {
+//     console.log(error, error.message);
+// }
 
 app.listen(PORT, () => {
     console.log(`server is running on PORT:${PORT}`);
 });
 
 
+
+app.get('/here', (req, res) =>{
+    console.log('here');
+    res.json('Hello world!');
+});
+
 // let a = document.getElementsByClassName('bfv-statistic')[0].getElementsByClassName('bfv-result-tile');
-// let last, next;
-// last = a[0].getElementsByClassName('bfv-matchdata-result__team-name bfv-matchdata-result__team-name--team0')[0].innerText;
-// console.log(last);
+// let home, away;
+// home = a[0].getElementsByClassName('bfv-matchdata-result__team-name bfv-matchdata-result__team-name--team0')[0].innerText;
+// away = a[0].getElementsByClassName('bfv-matchdata-result__team-name bfv-matchdata-result__team-name--team1')[0].innerText;
+// console.log(home, " -:- ", away);
+
+// Ermittle aus Vereinsseite erste Mannschaft aus der Liste
+// document.getElementsByClassName('bfv-team-list__entry-wrapper')[0].getElementsByClassName('bfv-composition-entry__content bfv-composition-entry__content--team-profilex')[0].querySelector('a').getAttribute('href')
