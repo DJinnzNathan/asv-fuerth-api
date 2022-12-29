c18d0c343f8e42cb7127049f1d25dc55fb70eca5
D-Jinnz Nathan <djinnz.nathan@gmail.com>
2022-12-29 01:27:56 +0100

    added number for experiment

--- test.js
+++ test.js
@@ -2,44 +2,72 @@ const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const app = express();
+const fs = require('fs/promises');
 
 const PORT = process.env.port || 3000;
 
-const targetURL = 'https://www.bfv.de/mannschaften/asv-fuerth-ii/016PH3NA80000000VV0AG80NVUT1FLRU';
+const targetURL = 'https://www.bfv.de/partial/mannschaftsprofil/spielplan/016PH3NA80000000VV0AG80NVUT1FLRU/letzte?wettbewerbsart=1&spieltyp=ALLE&from=5&size=5';
 
-const matches = ($) => {
+const test = ($) => {
     const content = [];
+    // .bfv-matchdata-result__goals--team0
+    const number = 6;
+    let temp = $('.bfv-matchdata-result__goals--team1')[1];
+    const key = temp.attribs['data-class-name'].replace('results-c-', '');
+    const character = temp.children[0].data.trim();
 
-    let temp = $('.bfv-statistic').find('.bfv-result-tile');
+    let text = key + ' : ' + character + '(' + character.charCodeAt(0) + ')' + '->' + number;
+    // ' : [character, value] }
+    let values = [{ [character]:number }];
+    let output = { [key]: values };
+    // output.values.push({ [character]:number });
+    // console.log(text);
+    content.push(output);
+    console.log(content);
 
-    temp.each((index, el) => {
-        const match = {};
+    // fetch('numbers.json').then(res => res.json()).then(data => {
+    //     data.push(output);
 
-        match.matchStatus = $(el).find('.bfv-result-tile__title').text();
-        match.date = '';
-        match.homeTeam = $(el).find('.bfv-matchdata-result__team-name--team0').text().trim();
-        match.awayTeam = $(el).find('.bfv-matchdata-result__team-name--team1').text().trim();
-        match.link = $(el).find('a').attr('href');
+    //     const json = JSON.stringify(data);
 
-        console.log(match);
-        content.push(match);
-    });
+    //     fs.writeFile('numbers.json', json, 'utf-8', (err) => {
+    //         if(err) throw err;
+    //         console.log('The file has been saved!');
+    //     });
+    // });
+
+
+    // temp.each((index, el) => {
+    //     const match = {};
+
+    //     match.matchStatus = $(el).find('.bfv-result-tile__title').text();
+    //     match.date = '';
+    //     match.homeTeam = $(el).find('.bfv-matchdata-result__team-name--team0').text().trim();
+    //     match.awayTeam = $(el).find('.bfv-matchdata-result__team-name--team1').text().trim();
+    //     match.link = $(el).find('a').attr('href');
+
+    //     console.log(match);
+    //     content.push(match);
+    // });
+    // addToReport('./entwicklung/auswertung.json', content);
     return content;
 };
 
-app.get('/matches', (req, res) => {
-
+app.get('/test', (req, res) => {
+    let $ = '';
     axios.get(targetURL).then((response) => {
         const body = response.data;
-        const $ = cheerio.load(body); // Load HTML data and initialize cheerio 
-        console.log("Matches: ", matches);
-        res.json(matches($));
+        $ = cheerio.load(body); // Load HTML data and initialize cheerio 
+        res.json(test($));
     });
 });
 
 app.listen(PORT, () => {
     console.log(`server is running on PORT:${PORT}`);
 });
+
+
+
 
 // NOTIZEN
 // let a = document.getElementsByClassName('bfv-statistic')[0].getElementsByClassName('bfv-result-tile');
