90428faa39ea5e2277fde71eedca9ffd20b025eb
D-Jinnz Nathan <40861395+DJinnzNathan@users.noreply.github.com>
2022-12-31 18:46:54 +0100

    DJinnzNathan/issue1 (#3)

--- test.js
+++ test.js
@@ -2,54 +2,57 @@ const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const app = express();
-const fs = require('fs/promises');
+const fs = require('fs');
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 3001;
 
 const targetURL = 'https://www.bfv.de/partial/mannschaftsprofil/spielplan/016PH3NA80000000VV0AG80NVUT1FLRU/letzte?wettbewerbsart=1&spieltyp=ALLE&from=5&size=5';
 
-const test = ($) => {
+
+function readFromJSONFile(filename) {
+    return JSON.parse(fs.readFileSync(filename));
+}
+
+function writeToJSONFile(filename, data) {
+    fs.writeFileSync(filename, JSON.stringify(data, null, 2));
+}
+
+function collectNumber(key, character, values) {
+    console.log('key: ', key);
+    console.log('character: ', character);
+    let data = readFromJSONFile('numbers.json');
+    console.log('if 1', data.some(item => item.hasOwnProperty(key)));
+    if (data.some(item => item.hasOwnProperty(key))) {
+        let pos = data.findIndex(item => item.hasOwnProperty(key));
+        console.log('Characters', data[pos][key]);
+        console.log('if 2', 'char exist:', !data[pos][key].some(item => item.hasOwnProperty(character)));
+        if (!data[pos][key].some(item => item.hasOwnProperty(character))) {
+            console.log('Teilweises Einfügen', data[pos][key]);
+            data[pos][key].push(values[0]);
+            writeToJSONFile('numbers.json', data);
+        }
+    } else {
+        console.log('key existiert nicht -> Kombi wird eingefügt');
+        data.push({ [key]: values });
+        writeToJSONFile('numbers.json', data);
+    }
+}
+
+function test($, row, number, homeTeam) {
     const content = [];
-    // .bfv-matchdata-result__goals--team0
-    const number = 6;
-    let temp = $('.bfv-matchdata-result__goals--team1')[1];
+    let teamClass = (homeTeam) ? '.bfv-matchdata-result__goals--team0' : '.bfv-matchdata-result__goals--team1';
+    const goalData = $(teamClass);
+    let temp = goalData[row];
     const key = temp.attribs['data-class-name'].replace('results-c-', '');
     const character = temp.children[0].data.trim();
+    console.log(key + ' : ' + character + '(' + character.charCodeAt(0) + ')' + '->' + number);
 
-    let text = key + ' : ' + character + '(' + character.charCodeAt(0) + ')' + '->' + number;
-    // ' : [character, value] }
-    let values = [{ [character]:number }];
+    let values = [{ [character]: number }];
     let output = { [key]: values };
-    // output.values.push({ [character]:number });
-    // console.log(text);
     content.push(output);
+    collectNumber(key, character, values);
+
     console.log(content);
-
-    // fetch('numbers.json').then(res => res.json()).then(data => {
-    //     data.push(output);
-
-    //     const json = JSON.stringify(data);
-
-    //     fs.writeFile('numbers.json', json, 'utf-8', (err) => {
-    //         if(err) throw err;
-    //         console.log('The file has been saved!');
-    //     });
-    // });
-
-
-    // temp.each((index, el) => {
-    //     const match = {};
-
-    //     match.matchStatus = $(el).find('.bfv-result-tile__title').text();
-    //     match.date = '';
-    //     match.homeTeam = $(el).find('.bfv-matchdata-result__team-name--team0').text().trim();
-    //     match.awayTeam = $(el).find('.bfv-matchdata-result__team-name--team1').text().trim();
-    //     match.link = $(el).find('a').attr('href');
-
-    //     console.log(match);
-    //     content.push(match);
-    // });
-    // addToReport('./entwicklung/auswertung.json', content);
     return content;
 };
 
@@ -58,8 +61,20 @@ app.get('/test', (req, res) => {
     axios.get(targetURL).then((response) => {
         const body = response.data;
         $ = cheerio.load(body); // Load HTML data and initialize cheerio 
-        res.json(test($));
+        res.json(test($, 1, 6, false));
     });
+});
+
+app.get('/m', (req, res) => {
+    const row = parseInt(req.query.row);
+    const goals = parseInt(req.query.goals);
+    const home = req.query.home;
+    axios.get(targetURL).then((response) => {
+        const body = response.data;
+        $ = cheerio.load(body); // Load HTML data and initialize cheerio
+        test($, row, goals, home);
+    });
+    res.send({ "row": row, "goals": goals, "home": home });
 });
 
 app.listen(PORT, () => {
